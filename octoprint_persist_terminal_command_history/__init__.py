cfc4b533a281d366fec53601362a5dc9a6c54726
Kevin Christensen <nivekmai@gmail.com>
2022-07-18 22:10:08 -0700

    Use separate config file for saving history
    Summary: Per the request on the PR (https://github.com/OctoPrint/plugins.octoprint.org/pull/1089#pullrequestreview-1042727001)
    
    Stores the history into a separate `json` file.
    
    Test plan:
    1. Installed new version (`octoprint dev plugin:install`)
    2. Entered a command in terminal and sent it
    3. Checked `%APPDATA%\OctoPrint\data\persist_terminal_command_history\command_history.json`
    4. Ensure history is correctly saved
    5. Reload OctoPrint
    6. History is correctly restored
    7. Repeat steps 2-7 to ensure saved/restored history is properly updated

--- octoprint_persist_terminal_command_history/__init__.py
+++ octoprint_persist_terminal_command_history/__init__.py
@@ -1,26 +1,32 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
+import json
 
 class PersistTerminalCommandHistoryPlugin(
     octoprint.plugin.SettingsPlugin,
     octoprint.plugin.AssetPlugin,
     octoprint.plugin.TemplatePlugin
 ):
+    def _get_file(self):
+        return self.get_plugin_data_folder() + "/command_history.json"
 
     ##~~ SettingsPlugin mixin
 
     def get_settings_defaults(self):
         return dict(persistedCmdHistory=[])
+
+    def on_settings_save(self, data):
+        with open(self._get_file(), "w") as outfile:
+            json.dump(data, outfile)
+    
+    def on_settings_load(self):
+        with open(self._get_file(), "r") as infile:
+            data = json.load(infile)
+            if data is not None:
+                return data
+            return self.get_settings_defaults()
 
     ##~~ AssetPlugin mixin
 
@@ -34,9 +40,6 @@ class PersistTerminalCommandHistoryPlugin(
     ##~~ Softwareupdate hook
 
     def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-        # for details.
         return {
             "persist_terminal_command_history": {
                 "displayName": "Persist Terminal Command History Plugin",
@@ -54,15 +57,8 @@ class PersistTerminalCommandHistoryPlugin(
         }
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Persist Terminal Command History Plugin"
 
-
-# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
-# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
-# OctoPrint 1.8.0 onwards only supports Python 3.
 __plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
 
 def __plugin_load__():
--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 # coding=utf-8
 
 ########################################################################################################################
-### Do not forget to adjust the following variables to your own plugin.
-
-# The plugin's identifier, has to be unique
 plugin_identifier = "persist_terminal_command_history"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
@@ -14,7 +11,7 @@ plugin_package = "octoprint_persist_terminal_command_history"
 plugin_name = "OctoPrint-PersistTerminalCommandHistory"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.2.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -35,32 +32,12 @@ plugin_license = "AGPLv3"
 # Any additional requirements besides OctoPrint should be listed here
 plugin_requires = []
 
-### --------------------------------------------------------------------------------------------------------------------
-### More advanced options that you usually shouldn't have to touch follow after this point
-### --------------------------------------------------------------------------------------------------------------------
-
-# Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
-# already be installed automatically if they exist. Note that if you add something here you'll also need to update
-# MANIFEST.in to match to ensure that python setup.py sdist produces a source distribution that contains all your
-# files. This is sadly due to how python's setup.py works, see also http://stackoverflow.com/a/14159430/2028598
 plugin_additional_data = []
 
-# Any additional python packages you need to install with your plugin that are not contained in <plugin_package>.*
 plugin_additional_packages = []
 
-# Any python packages within <plugin_package>.* you do NOT want to install with your plugin
 plugin_ignored_packages = []
 
-# Additional parameters for the call to setuptools.setup. If your plugin wants to register additional entry points,
-# define dependency links or other things like that, this is the place to go. Will be merged recursively with the
-# default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
-# octoprint.util.dict_merge.
-#
-# Example:
-#     plugin_requires = ["someDependency==dev"]
-#     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
-# "python_requires": ">=3,<4" blocks installation on Python 2 systems, to prevent confused users and provide a helpful error. 
-# Remove it if you would like to support Python 2 as well as 3 (not recommended).
 additional_setup_parameters = {"python_requires": ">=3,<4"}
 
 ########################################################################################################################
