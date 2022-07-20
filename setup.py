3fa52c20943826599bc6d595d1e2c5f7b4d3f167
Kevin Christensen <nivekmai@gmail.com>
2022-07-20 06:37:33 -0700

    Clean up cookie cutter files/comments

--- setup.py
+++ setup.py
@@ -3,33 +3,22 @@
 ########################################################################################################################
 plugin_identifier = "persist_terminal_command_history"
 
-# The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_persist_terminal_command_history"
 
-# The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
-# plugin module
 plugin_name = "OctoPrint-PersistTerminalCommandHistory"
 
-# The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.2.0"
 
-# The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
-# module
 plugin_description = """Ties into the default terminal plugin to save command history (press up to replay old commands) across reloads."""
 
-# The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Kevin Christensen"
 
-# The plugin's author's mail address.
 plugin_author_email = "nivekmai@gmail.com"
 
-# The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
 plugin_url = "https://github.com/nivekmai/OctoPrint-PersistTerminalCommandHistory"
 
-# The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
 
-# Any additional requirements besides OctoPrint should be listed here
 plugin_requires = []
 
 plugin_additional_data = []
